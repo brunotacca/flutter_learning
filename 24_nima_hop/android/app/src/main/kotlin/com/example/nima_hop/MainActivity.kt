c3a43746c63e1bd63d0b24808219fbf8ca722e6f
Tacca <brunotacca@gmail.com>
2020-07-28 11:00:56 -0300

    new examples

---
+++ 24_nima_hop/android/app/src/main/kotlin/com/example/nima_hop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nima_hop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 25_nima_ik/android/app/src/main/kotlin/com/example/nima_ik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.nima_ik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
