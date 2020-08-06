fb3a1cf2f2919ee047e3f8c4d6902f4dc6cb3815
Tacca <brunotacca@gmail.com>
2020-08-06 18:04:46 -0300

    improving screens

--- 31_safe_storage_biometric_encryption/lib/fingerprint_screen.dart
+++ 31_safe_storage_biometric_encryption/lib/fingerprint_screen.dart
@@ -1,6 +1,14 @@
 import 'package:biometric_storage/biometric_storage.dart';
+import 'package:crypton/crypton.dart';
+import 'package:device_info/device_info.dart';
 import 'package:flutter/material.dart';
 import 'package:safe_storage_biometric_encryption/welcome_screen.dart';
+import 'dart:io';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FringerPrintScreen extends StatefulWidget {
   @override
@@ -8,13 +16,156 @@ class FringerPrintScreen extends StatefulWidget {
 }
 
 class _FringerPrintScreenState extends State<FringerPrintScreen> {
-  BiometricStorageFile _customPrompt;
+  BiometricStorageFile storageFile;
+  static final DeviceInfoPlugin deviceInfoPlugin = DeviceInfoPlugin();
+  Map<String, String> _actualDeviceData = <String, String>{};
+  String _actualDeviceFingerprint = "";
+  final TextEditingController _writeController =
+      TextEditingController(text: '');
+  Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+  bool _newUser = true;
+  String _customMessage = "";
+  Color __customMessageColor = Colors.blueAccent;
+  String _rsaPublic;
 
   @override
   void initState() {
     super.initState();
 
+    _initStorageSecurityControl();
     _checkAuthenticate();
+  }
+
+  Future<bool> _authenticate(dynamic resultJson) async {
+    if (resultJson['rsaPublic'] != _rsaPublic) {
+      await storageFile.delete();
+      return false;
+    }
+
+    RSAPrivateKey priv = RSAPrivateKey.fromString(resultJson['rsaPrivate']);
+
+    String dfp = priv.decrypt(resultJson['deviceFingerprint']);
+    if (dfp != _actualDeviceFingerprint) {
+      await storageFile.delete();
+      return false;
+    }
+
+    String smth = priv.decrypt(resultJson['something']);
+    if (smth != _writeController.text) {
+      return false;
+    }
+
+    print("WELCOME BACK!");
+
+    return true;
+  }
+
+  Future<Map<String, String>> _generateNewUserData() async {
+    Map<String, String> newUserData = <String, String>{};
+
+    var rsaKeypair = RSAKeypair.fromRandom();
+    newUserData['rsaPrivate'] = rsaKeypair.privateKey.toString();
+    newUserData['rsaPublic'] = rsaKeypair.publicKey.toString();
+    newUserData['deviceFingerprint'] =
+        rsaKeypair.publicKey.encrypt(_actualDeviceFingerprint);
+    newUserData['something'] =
+        rsaKeypair.publicKey.encrypt(_writeController.text);
+
+    final SharedPreferences prefs = await _prefs;
+    await prefs.setString('rsaPublic', newUserData['rsaPublic']);
+    _rsaPublic = prefs.getString("rsaPublic");
+
+    return newUserData;
+  }
+
+  Future<void> _initStorageSecurityControl() async {
+    final SharedPreferences prefs = await _prefs;
+
+    // Check for Existing RSA Public Key
+    _rsaPublic = prefs.getString("rsaPublic");
+    bool newbie = true;
+    if (_rsaPublic != null && _rsaPublic.length > 0) {
+      newbie = false;
+    }
+
+    // Get device info
+    _actualDeviceData = await _initPlatformState();
+    _actualDeviceFingerprint = _actualDeviceData.values.join();
+
+    if (!mounted) return;
+    setState(() {
+      _newUser = newbie;
+    });
+
+    /*
+    print("_deviceFingerprint: $_deviceFingerprint");
+    String _deviceFingerprint32b =
+        _deviceFingerprint.substring(_deviceFingerprint.length - 10);
+    print("_deviceFingerprint32b: $_deviceFingerprint32b");
+    final b64key = encr.Key.fromUtf8(
+        base64Url.encode(encr.Key.fromUtf8(_deviceFingerprint32b).bytes));
+    print("b64key ${b64key.base64}");
+    final fernet = encr.Fernet(b64key);
+    _encrypter = encr.Encrypter(fernet);
+    encr.Encrypted encrypted = _encrypter.encrypt("HELLO WORLD");
+    print("HELLO WORLD ENCRYPTED: " + encrypted.base64);
+    print("HELLO WORLD DECRYPTED: " + _encrypter.decrypt(encrypted));
+    */
+    /*
+    var rsaKeypair = RSAKeypair.fromRandom();
+    var message = "Hello World!";
+
+    var privateKeyString = rsaKeypair.privateKey.toString();
+    var publicKeyString = rsaKeypair.publicKey.toString();
+    var encrypted = rsaKeypair.publicKey.encrypt(message);
+    var decrypted = rsaKeypair.privateKey.decrypt(encrypted);
+
+    print('Your Private Key\n $privateKeyString\n---');
+    print('Your Public Key\n $publicKeyString\n---');
+    print('Encrypted Message\n $encrypted\n---');
+    print('Decrypted Message\n $decrypted\n---');
+
+    if (decrypted == message) {
+      print('The Message was successfully decrypted!');
+    } else {
+      print('Failed to decrypted the Message!');
+    }
+    */
+  }
+
+  Future<Map<String, String>> _initPlatformState() async {
+    Map<String, String> deviceData;
+
+    try {
+      if (Platform.isAndroid) {
+        deviceData = _readAndroidBuildData(await deviceInfoPlugin.androidInfo);
+      } else if (Platform.isIOS) {
+        deviceData = _readIosDeviceInfo(await deviceInfoPlugin.iosInfo);
+      }
+    } on PlatformException {
+      deviceData = <String, String>{
+        'Error:': 'Failed to get platform version.'
+      };
+    }
+
+    return deviceData;
+  }
+
+  Map<String, String> _readAndroidBuildData(AndroidDeviceInfo build) {
+    return <String, String>{
+      'board': build.board,
+      'device': build.device,
+      'fingerprint': build.fingerprint,
+      'androidId': build.androidId,
+    };
+  }
+
+  Map<String, String> _readIosDeviceInfo(IosDeviceInfo data) {
+    return <String, String>{
+      'systemName': data.systemName,
+      'model': data.model,
+      'identifierForVendor': data.identifierForVendor,
+    };
   }
 
   Future<CanAuthenticateResponse> _checkAuthenticate() async {
@@ -23,13 +174,25 @@ class _FringerPrintScreenState extends State<FringerPrintScreen> {
 
     return Future.delayed(
       Duration(seconds: 2),
-      () {
-        if (response == CanAuthenticateResponse.success) {
+      () async {
+        if (response != CanAuthenticateResponse.success) {
           Navigator.pushReplacement(context,
               MaterialPageRoute(builder: (context) => WelcomeScreen()));
           return null;
-        } else
+        } else {
+          storageFile = await BiometricStorage().getStorage(
+            'biometricStorageFile',
+            options: StorageFileInitOptions(
+                authenticationValidityDurationSeconds: 60),
+            androidPromptInfo: const AndroidPromptInfo(
+              title: 'YOU',
+              subtitle: 'SHALL NOT',
+              description: 'PASS!',
+              negativeButton: 'Okey!',
+            ),
+          );
           return response;
+        }
       },
     );
   }
@@ -44,7 +207,7 @@ class _FringerPrintScreenState extends State<FringerPrintScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Text(
-                "There is nothing here.",
+                _newUser ? "There is nothing here." : "Welcome back!",
                 style: TextStyle(
                     fontSize: 18,
                     decoration: TextDecoration.none,
@@ -62,19 +225,134 @@ class _FringerPrintScreenState extends State<FringerPrintScreen> {
                     );
                   } else
                     return SizedBox(
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.lock,
-                          color: Colors.blueAccent,
-                          size: 40,
+                      child: Hero(
+                        tag: 'lock',
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.lock,
+                            color: Colors.blueAccent,
+                            size: 40,
+                          ),
+                          onPressed: () async {
+                            print("UNLOCK");
+                            if (_writeController.text != null &&
+                                _writeController.text.isNotEmpty) {
+                              try {
+                                final result = await storageFile.read();
+                                final resultJson =
+                                    result != null ? json.decode(result) : null;
+
+                                if (result == null ||
+                                    resultJson == null ||
+                                    resultJson['deviceFingerprint'] == null) {
+                                  Map<String, String> data =
+                                      await _generateNewUserData();
+                                  await storageFile.write(json.encode(data));
+
+                                  setState(() {
+                                    _writeController.text = "";
+                                    _customMessage =
+                                        "Welcome New User!\nType your something and authenticate again!";
+                                    __customMessageColor = Colors.greenAccent;
+                                  });
+
+                                  print("FIRST ENTRY! WRITED!");
+                                } else {
+                                  final SharedPreferences prefs = await _prefs;
+                                  if (_rsaPublic == null)
+                                    await _initStorageSecurityControl();
+
+                                  bool a = await _authenticate(resultJson);
+
+                                  if (!a) {
+                                    _writeController.text = "";
+                                    _customMessage = "¯\\_(ツ)_/¯";
+                                    __customMessageColor = Colors.red;
+                                  } else {
+                                    _writeController.text = "( ͡° ͜ʖ ͡°)";
+                                    _customMessage = "YOU MAY ENTER...";
+                                    __customMessageColor = Colors.greenAccent;
+                                    Future.delayed(Duration(seconds: 2), () {
+                                      Navigator.pushReplacement(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  WelcomeScreen()));
+                                    });
+                                  }
+
+                                  setState(() {});
+
+                                  print("WELCOME BACK! $a");
+                                }
+                              } catch (e) {
+                                print("Error: $e");
+                                setState(() {
+                                  _writeController.text = "";
+                                });
+                              }
+                            }
+                          },
                         ),
-                        onPressed: () {
-                          print("UNLOCK");
-                        },
                       ),
                     );
                 },
               ),
+              Divider(),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Text(
+                      ">",
+                      style: TextStyle(
+                          fontSize: 18,
+                          decoration: TextDecoration.none,
+                          color: Colors.blueAccent),
+                    ),
+                  ),
+                  Expanded(
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        labelText: 'Tell me something',
+                      ),
+                      style: TextStyle(color: Colors.blueAccent),
+                      textAlign: TextAlign.center,
+                      controller: _writeController,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: Text(
+                      "<",
+                      style: TextStyle(
+                          fontSize: 18,
+                          decoration: TextDecoration.none,
+                          color: Colors.blueAccent),
+                    ),
+                  ),
+                ],
+              ),
+              _customMessage.isEmpty
+                  ? Container()
+                  : Padding(
+                      padding: const EdgeInsets.only(top: 10),
+                      child: Text(
+                        _customMessage,
+                        style: TextStyle(color: __customMessageColor),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+              Divider(),
+              RaisedButton(
+                child: Text("Wipe Storage"),
+                onPressed: () async {
+                  await storageFile.delete();
+                  final SharedPreferences prefs = await _prefs;
+                  prefs.setString("rsaPublic", "");
+                  print("DELETED");
+                },
+              )
             ],
           ),
         ),
--- 31_safe_storage_biometric_encryption/lib/welcome_screen.dart
+++ 31_safe_storage_biometric_encryption/lib/welcome_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:passcode_screen/circle.dart';
+import 'package:passcode_screen/keyboard.dart';
+import 'package:passcode_screen/passcode_screen.dart';
 import 'package:safe_storage_biometric_encryption/yeti.dart';
+import 'package:flutter/scheduler.dart';
 
 class WelcomeScreen extends StatefulWidget {
   @override
@@ -7,8 +13,23 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  final StreamController<bool> _verificationNotifier =
+      StreamController<bool>.broadcast();
+
+  final TextEditingController _writeController =
+      TextEditingController(text: '');
+
+  bool isAuthenticated = false;
+
+  @override
+  void dispose() {
+    _verificationNotifier.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    timeDilation = 3.0;
     return Scaffold(
       body: Stack(
         alignment: Alignment.center,
@@ -17,23 +38,108 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           Positioned(
             top: MediaQuery.of(context).size.center(Offset.zero).dy + 57,
             child: InkWell(
-              child: ClipOval(
-                child: Container(
-                  color: Color(0xFF60C7D0),
-                  child: const Icon(
-                    Icons.lock,
-                    color: Colors.black,
-                    size: 25,
+              onTap: () {
+                _showLockScreen(
+                  context,
+                  opaque: false,
+                  circleUIConfig: CircleUIConfig(
+                      borderColor: Colors.blue,
+                      fillColor: Colors.blue,
+                      circleSize: 30),
+                  keyboardUIConfig: KeyboardUIConfig(
+                      digitBorderWidth: 2, primaryColor: Colors.blue),
+                  cancelButton: Icon(
+                    Icons.arrow_back,
+                    color: Colors.blue,
+                  ),
+                  digits: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '❤']
+                    ..shuffle(),
+                );
+              },
+              child: Hero(
+                tag: 'lock',
+                child: ClipOval(
+                  child: Container(
+                    color: Color(0xFF60C7D0),
+                    child: const Icon(
+                      Icons.lock,
+                      color: Colors.black,
+                      size: 25,
+                    ),
                   ),
                 ),
               ),
-              onTap: () {
-                print("Q");
-              },
             ),
           ),
+          Positioned(
+            top: MediaQuery.of(context).size.center(Offset.zero).dy + 180,
+            child: Text("There might be something hidden here..."),
+          ),
+          isAuthenticated
+              ? Container()
+              : Positioned(
+                  top: MediaQuery.of(context).size.center(Offset.zero).dy + 210,
+                  child: Row(
+                    children: [
+                      Text(">"),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: TextField(),
+                      ),
+                      Text("<"),
+                    ],
+                  ),
+                ),
         ],
       ),
     );
+  }
+
+  _showLockScreen(BuildContext context,
+      {bool opaque,
+      CircleUIConfig circleUIConfig,
+      KeyboardUIConfig keyboardUIConfig,
+      Widget cancelButton,
+      List<String> digits}) {
+    Navigator.push(
+        context,
+        PageRouteBuilder(
+          opaque: opaque,
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              PasscodeScreen(
+            title: Text(
+              'Secret PIN',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+            circleUIConfig: circleUIConfig,
+            keyboardUIConfig: keyboardUIConfig,
+            passwordEnteredCallback: _onPasscodeEntered,
+            cancelButton: cancelButton,
+            deleteButton: Text(
+              'Delete',
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+              semanticsLabel: 'Delete',
+            ),
+            shouldTriggerVerification: _verificationNotifier.stream,
+            backgroundColor: Colors.black.withOpacity(0.8),
+            cancelCallback: _onPasscodeCancelled,
+            digits: digits,
+          ),
+        ));
+  }
+
+  _onPasscodeEntered(String enteredPasscode) {
+    bool isValid = '12345❤' == enteredPasscode;
+    _verificationNotifier.add(isValid);
+    if (isValid) {
+      setState(() {
+        this.isAuthenticated = isValid;
+      });
+    }
+  }
+
+  _onPasscodeCancelled() {
+    Navigator.maybePop(context);
   }
 }
